88709e1df6e066e135797171e6b73552b14ad8b9
Arnau478 <arnauxabia@gmail.com>
2021-12-26 03:53:11 +0100

    Binary operators bytecode

--- vm.c
+++ vm.c
@@ -31,6 +31,14 @@ Value pop(){
 static InterpretResult run(){
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do{ \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } \
+    while(false);
+
     for(;;){
 
 #ifdef DEBUG_TRACE_EXECUTION
@@ -51,6 +59,22 @@ static InterpretResult run(){
                 push(constant);
                 break;
             }
+            case OP_ADD: {
+                BINARY_OP(+);
+                break;
+            }
+            case OP_SUBTRACT: {
+                BINARY_OP(-);
+                break;
+            }
+            case OP_MULTIPLY: {
+                BINARY_OP(*);
+                break;
+            }
+            case OP_DIVIDE: {
+                BINARY_OP(/);
+                break;
+            }
             case OP_NEGATE: {
                 push(-pop());
                 break;
@@ -64,6 +88,7 @@ static InterpretResult run(){
     }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk){
