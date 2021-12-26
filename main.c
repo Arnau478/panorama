3a0b7dcf647bd7c3892b09dca8cf376960d250a8
Arnau478 <arnauxabia@gmail.com>
2021-12-26 02:35:36 +0100

    Basic VM

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
-#include "value.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]){
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
@@ -14,6 +16,8 @@ int main(int argc, const char* argv[]){
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
 
     return 0;
